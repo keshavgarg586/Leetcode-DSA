963196cddcff11b74f20166680f96a6f00b30068
KESHAV GARG <keshavgarg586@gmail.com>
2026-08-03 18:48:33 +0530

    Time: 0 ms (100%), Space: 27.7 MB (28.86%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.cpp
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -1,27 +1,40 @@
+// class Solution {
+// public:
+//     bool isArraySpecial(vector<int>& nums) {
+//         bool ans=false;;
+//         int ans1=1;
+//         int ans2=0;
+
+//         if (nums.size()==1)
+//         return true;
+
+//         for(int i=0;i<nums.size()-1;i++)
+//         {
+//             ans1=nums[i]%2;
+//             ans2=nums[i+1]%2;
+
+//             if(ans1==1 && ans2==0 || ans1==0 && ans2==1)
+//             ans=true;
+//             else{
+//                 ans=false;
+//                 break;
+//             }
+            
+
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        bool ans=false;;
-        int ans1=1;
-        int ans2=0;
-
-        if (nums.size()==1)
-        return true;
-
-        for(int i=0;i<nums.size()-1;i++)
-        {
-            ans1=nums[i]%2;
-            ans2=nums[i+1]%2;
-
-            if(ans1==1 && ans2==0 || ans1==0 && ans2==1)
-            ans=true;
-            else{
-                ans=false;
-                break;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            // Bitwise check: if both elements have the same parity, return false
+            if ((nums[i] % 2) == (nums[i + 1] % 2)) {
+                return false;
             }
-            
-
         }
-        return ans;
+        return true;
     }
 };
