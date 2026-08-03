3ee67856e7009c83d0c1e18346e35bbd056915bd
KESHAV GARG <keshavgarg586@gmail.com>
2026-08-03 18:44:54 +0530

    Time: 0 ms (100%), Space: 27.8 MB (28.86%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        bool ans=false;;
+        int ans1=1;
+        int ans2=0;
+
+        if (nums.size()==1)
+        return true;
+
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            ans1=nums[i]%2;
+            ans2=nums[i+1]%2;
+
+            if(ans1==1 && ans2==0 || ans1==0 && ans2==1)
+            ans=true;
+            else{
+                ans=false;
+                break;
+            }
+            
+
+        }
+        return ans;
+    }
+};
